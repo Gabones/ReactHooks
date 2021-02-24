9fd9ec669e406bff1384f125f09fcea2596e3a5e
Gabriel Albuquerque da Rosa <gabaoalb@hotmail.com>
2021-02-24 18:05:03 -0300

    use Effect

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+const calcFatorial = s => {
+    const n = parseInt(s)
+    if(n < 0) return -1
+    if(n===0) return 1
+    return calcFatorial(n-1)*n
+}
 
 const UseEffect = (props) => {
+
+    const [number, setNumber] = useState(1)
+    const [fatorial, setFatorial] = useState(1)
+    const [parOuImpar, setParOuImpar] = useState(1)
+
+    useEffect(function() {setFatorial(calcFatorial(number))}, [number])
+
+    useEffect(function() {if(fatorial > 1000000) document.title = 'High Number'}, [fatorial])
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <SectionTitle title="Exercício #01"></SectionTitle>
+            <div>
+                <span className="text">Fatorial: </span>
+                <span className="text red">{fatorial === -1 ? 'Não Existe' : fatorial}</span>
+            </div>
+            <div>
+                <input type="number" className="input" value={number} onChange={e => setNumber(+e.target.value)}/>
+            </div>
+            <SectionTitle title="Exercício #02"></SectionTitle>
+            <div>
+                <span className="text">Paridade: </span>
+                <span className="text red">{parOuImpar % 2 === 0 ? 'Par' : 'Ímpar'}</span>
+            </div>
+            <div>
+                <input type="number" className="input" value={parOuImpar} onChange={e => setParOuImpar(+e.target.value)}/>
+            </div>
         </div>
     )
 }
